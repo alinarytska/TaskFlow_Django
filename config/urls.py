be8f1ae105543ec82fb7e2ef854e12bc9ef8ae1d
Alina_Rytska <alinarytskade@gmail.com>
2026-05-06 22:17:42 +0200

    Complete Homework_7

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from test_app.views import greetings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home-page/', greetings),
 ]
