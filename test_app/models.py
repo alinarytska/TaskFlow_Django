857f8b790ed342f8b4c6ddcc3ad72d0751954e55
Alina_Rytska <alinarytskade@gmail.com>
2026-05-17 20:23:29 +0200

    Homework_8

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+status_choices = [
+    ('new', 'New'),
+    ('in_progress', 'In progress'),
+    ('pending', 'Pending'),
+    ('blocked', 'Blocked'),
+    ('done', 'Done'),
+]
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, unique_for_date='created_at')
+    description = models.TextField()
+    categories = models.ManyToManyField('Category')
+    status = models.CharField(max_length=50, choices=status_choices)
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SubTask(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    status = models.CharField(max_length=50, choices=status_choices)
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
